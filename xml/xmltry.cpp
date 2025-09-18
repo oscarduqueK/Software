b80386d4674f27c764fef6b8381367ab3973b466
OscarDukek <oscar.duque@enti.cat>
2025-09-18 17:00:05 +0200

    xmls

--- xml/xmltry.cpp
+++ xml/xmltry.cpp
@@ -1,3 +1,11 @@
+#include "dep/inc/xml/rapidxml.hpp"
+#include "dep/inc/xml/rapidxml_print.hpp"
+#include "dep/inc/xml/rapidxml_utils.hpp"
+#include "dep/inc/xml/rapidxml_iterators.hpp"
+
+#include <iostream>
+#include <sstream> 
+
 int main()
 {
 	return 0;
